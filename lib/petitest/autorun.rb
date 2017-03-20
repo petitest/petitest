ef9b1741f0f6351c0654fc28c22abc33ca53407e
r7kamura <r7kamura@gmail.com>
2017-03-21 06:00:36 +0900

    Use ternary operator

--- lib/petitest/autorun.rb
+++ lib/petitest/autorun.rb
@@ -3,12 +3,6 @@ require "petitest"
 at_exit do
   test_cases = Petitest::TestCasesCollector.singleton.collect
   result = Petitest::TestCasesRunner.new(test_cases).run
-  exit_code = begin
-    if result
-      0
-    else
-      1
-    end
-  end
+  exit_code = result ? 0 : 1
   exit(exit_code)
 end
