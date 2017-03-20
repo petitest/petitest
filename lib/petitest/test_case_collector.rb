e61ec0a5f8f75b999cd4e2b70e9e5b5945683f0c
r7kamura <r7kamura@gmail.com>
2017-03-21 05:13:30 +0900

    Remove unused code

--- lib/petitest/test_case_collector.rb
+++ lib/petitest/test_case_collector.rb
@@ -1,10 +1,7 @@
 module Petitest
   class TestCaseCollector
-    TEST_FILES_PATH_PATTERN = "test/**/*_test.rb"
-
     # @return [Array<Petit::TestCase]
     def collect
-      # require_test_files
       ::Petitest::TestGroup.descendants.flat_map do |test_group_class|
         test_group_class.test_methods.map do |test_method|
           ::Petitest::TestCase.new(
@@ -12,19 +9,6 @@ module Petitest
             test_method: test_method,
           )
         end
-      end
-    end
-
-    private
-
-    # @return [Array<String>]
-    def collect_test_file_paths
-      ::Dir.glob(TEST_FILES_PATH_PATTERN)
-    end
-
-    def require_test_files
-      collect_test_file_paths.each do |path|
-        require ::File.expand_path(path)
       end
     end
   end
