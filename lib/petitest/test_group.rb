3075e408e681432e6a2ad42695dfb78574e979b8
r7kamura <r7kamura@gmail.com>
2017-03-21 08:43:43 +0900

    Refactor: add test_method_names method

--- lib/petitest/test_group.rb
+++ lib/petitest/test_group.rb
@@ -16,11 +16,16 @@ module Petitest
         descendants << sub_class
       end
 
-      # @return [Array<Petitest::TestMethod>]
-      def test_methods
+      # @return [Array<String>]
+      def test_method_names
         public_instance_methods.map(&:to_s).select do |method_name|
           method_name.start_with?(TEST_METHOD_NAME_PREFIX)
-        end.map do |method_name|
+        end
+      end
+
+      # @return [Array<Petitest::TestMethod>]
+      def test_methods
+        test_method_names.map do |method_name|
           unbound_method = public_instance_method(method_name)
           ::Petitest::TestMethod.new(
             line_number: unbound_method.source_location[1],
