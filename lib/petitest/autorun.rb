22d7b443d714aa7fb29b09ed19ff8c07b1666e0d
r7kamura <r7kamura@gmail.com>
2017-03-24 01:00:51 +0900

    Stop tests if any error occured before running tests

--- lib/petitest/autorun.rb
+++ lib/petitest/autorun.rb
@@ -1,6 +1,9 @@
 require "petitest"
 
 at_exit do
+  if $! && !($!.is_a?(::SystemExit) && $!.success?)
+    next
+  end
   test_cases = Petitest::TestGroup.test_cases
   result = Petitest::TestCasesRunner.new(test_cases).run
   exit_code = result ? 0 : 1
