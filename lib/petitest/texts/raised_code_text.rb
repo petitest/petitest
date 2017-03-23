0d73e3b2d6f80cdd24ba387beb45f664048ed77e
r7kamura <r7kamura@gmail.com>
2017-03-24 00:59:44 +0900

    Fix bug: NoMethodError with empty backtrace

--- lib/petitest/texts/raised_code_text.rb
+++ lib/petitest/texts/raised_code_text.rb
@@ -24,24 +24,32 @@ module Petitest
 
       # @return [String, nil]
       def caller_file_content
-        if ::FileTest.file?(caller_path)
+        if caller_path && ::FileTest.file?(caller_path)
           ::File.read(caller_path)
         end
       end
 
-      # @return [String]
+      # @return [String, nil]
       def caller_line_number
-        caller_segments[1].to_i
+        if caller_segments[1]
+          caller_segments[1].to_i
+        end
       end
 
-      # @return [String]
+      # @return [String, nil]
       def caller_path
         caller_segments[0]
       end
 
       # @return [Array<String>]
       def caller_segments
-        @caller_segments ||= test_case.filtered_backtrace[0].split(":", 3)
+        @caller_segments ||= begin
+          if test_case.filtered_backtrace[0]
+            test_case.filtered_backtrace[0].split(":", 3)
+          else
+            []
+          end
+        end
       end
     end
   end
