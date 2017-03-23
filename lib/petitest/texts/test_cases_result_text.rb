a24fe09523823e0281845741bf59717a1e463889
r7kamura <r7kamura@gmail.com>
2017-03-24 01:36:13 +0900

    Ignore empty errors information on report

--- lib/petitest/texts/test_cases_result_text.rb
+++ lib/petitest/texts/test_cases_result_text.rb
@@ -28,17 +28,39 @@ module Petitest
       # @note Override
       def to_s
         [
-          ::Petitest::Texts::TestCasesResultMarginTopText.new(test_cases: test_cases),
-          [
-            ::Petitest::Texts::FailuresText.new(test_cases: test_cases.select(&:failed?)),
-            ::Petitest::Texts::ErrorsText.new(test_cases: test_cases.select(&:aborted?)),
-            ::Petitest::Texts::TestCountsText.new(test_cases: test_cases),
-            ::Petitest::Texts::TimesText.new(
-              finished_at: finished_at,
-              started_at: started_at,
-            ),
-          ].join("\n\n"),
+          header,
+          body,
         ].join
+      end
+
+      private
+
+      # @return [String]
+      def body
+        texts = []
+        texts << ::Petitest::Texts::FailuresText.new(test_cases: test_cases_failed) unless test_cases_failed.empty?
+        texts << ::Petitest::Texts::ErrorsText.new(test_cases: test_cases_aborted) unless test_cases_aborted.empty?
+        texts << ::Petitest::Texts::TestCountsText.new(test_cases: test_cases)
+        texts << ::Petitest::Texts::TimesText.new(
+          finished_at: finished_at,
+          started_at: started_at,
+        )
+        texts.join("\n\n")
+      end
+
+      # @return [String]
+      def header
+        ::Petitest::Texts::TestCasesResultMarginTopText.new(test_cases: test_cases).to_s
+      end
+
+      # @return [Array<Petitest::TestCase>]
+      def test_cases_aborted
+        @test_cases_aborted ||= test_cases.select(&:aborted?)
+      end
+
+      # @return [Array<Petitest::TestCase>]
+      def test_cases_failed
+        @test_cases_failed ||= test_cases.select(&:failed?)
       end
     end
   end
